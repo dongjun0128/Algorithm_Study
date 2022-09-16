b696c19b3b1b6d5449d77588d4d6c92c5a4ff6f0
dongjun0128 <dongjun9701@naver.com>
2022-09-16 10:50:20 +0900

    Update 4. 가장 흔한 단어.py

--- Python_Algorithm_Interview/문자열 조작/4. 가장 흔한 단어.py
+++ Python_Algorithm_Interview/문자열 조작/4. 가장 흔한 단어.py
@@ -1,12 +1,17 @@
 import collections
 import re
+from typing import List
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
         
+        #re.sub(r'[^\w]',' ',paragraph) 에서 \w는 단어 문자를 뜻하며, ^는 not을 의미
+        #re.sub(r'[^\w]',' ',paragraph)의 의미는 paragraph에서 단어 문자가 아닌 모든 문자를 공백으로 치환
         words=[word for word in re.sub(r'[^\w]',' ',paragraph).lower().split() 
                if word not in banned]
-        
+
+        #딕셔너리 형태로 단어가 몇 번 나왔는지 세준다.
         counts = collections.Counter(words)
         
+        #가장 많이 나온 단어를 출력
         return (counts.most_common(1)[0][0])
