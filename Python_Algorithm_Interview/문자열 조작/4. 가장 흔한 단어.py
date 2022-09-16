ae5c9c95bf4044336232d644e845b43a982f33d7
dongjun0128 <dongjun9701@naver.com>
2022-09-16 10:44:39 +0900

    Create 4. 가장 흔한 단어.py

---
+++ Python_Algorithm_Interview/문자열 조작/4. 가장 흔한 단어.py
@@ -0,0 +1,12 @@
+import collections
+import re
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        
+        words=[word for word in re.sub(r'[^\w]',' ',paragraph).lower().split() 
+               if word not in banned]
+        
+        counts = collections.Counter(words)
+        
+        return (counts.most_common(1)[0][0])
